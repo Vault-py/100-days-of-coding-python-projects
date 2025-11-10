ed109fb9c9c63b121f9e783690313702e6f6bbd4
FusionTech000 <propertydreal@gmail.com>
2025-11-10 23:57:19 +0100

    i created a variable to store the users name and then created a variable to store the length of the variable used to store the users name

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # practice tasks
+# checking the length of the users name
+#print(len(input("Whats your name")))
 
-print("Hello " + input("Whats your name")+ "!")
+
+#i created a variable to store the users name and then created a variable to store the length of the variable used to store the users name
+
+users_name = input("Whats your name")
+
+
+length_users_name = len(users_name)
+
+print(length_users_name)
